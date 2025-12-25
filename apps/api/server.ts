66a688ea3ea6ae5fd7b40490ae4d22ce0d425a12
Kane Vidzro <kanevidzro@gmail.com>
2025-12-25 11:26:22 +0000

    Update server.ts

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -3,7 +3,15 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text("Hello Dugble API!");
+  return c.text("", 404); 
+});
+
+app.get("/v1/test", (c) => {
+  return c.json({ message: "This endpoint works!" });
+});
+
+app.notFound((c) => {
+  return c.text("", 404);
 });
 
 export default {
